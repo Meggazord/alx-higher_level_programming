fe992e138b53e84664254a1bcdba74fe8c9d6fcf
Meggazord <k_megahed@hotmail.com>
2023-11-30 01:10:35 +0200

    solved

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    print("{:c}".format(i if i % 2 == 0 else i - 32), end="")
+for i in range(90, 64, -1):
+    print("{}".format(chr(i + (i % 2) * 32)), end="")
