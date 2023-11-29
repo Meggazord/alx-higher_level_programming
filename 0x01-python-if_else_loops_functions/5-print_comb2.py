3256e394ce0dc97dc6955aa8bb975fae7c0339e9
Meggazord <k_megahed@hotmail.com>
2023-11-30 00:52:41 +0200

    solved

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for x in range(0, 99):
-    print("{}".format(x: 02d), end=", ")
-print("{}".format((x + 1): 02d))
+    print("{:02d}".format(x), end=", ")
+print("{: 02d}".format((x + 1)))
