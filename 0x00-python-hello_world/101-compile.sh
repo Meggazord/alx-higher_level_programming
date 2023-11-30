16e53bef657eb50237112424658f91960e1c55c1
Meggazord <k_megahed@hotmail.com>
2023-11-30 13:32:41 +0200

    fixed

--- 0x00-python-hello_world/101-compile.sh
+++ 0x00-python-hello_world/101-compile.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python3 -m compileall -b $PYFILE i
+
+if [ -z "$PYFILE" ]; then
+  echo "Error: PYFILE environment variable is not set."
+  exit (1)
+fi
+
+echo "Compiling $PYFILE ..."
+python3 -m py_compile "$PYFILE"
+
+exit (0)
