05d3a6c2755b348737dc8a62c4f377e39a49b389
Meggazord <k_megahed@hotmail.com>
2023-12-04 17:45:29 +0200

    solved more

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''
+    new_string = ""
     for i in my_string:
-        if i != 'c' or i != 'C':
+        if i != 'c' and i != 'C':
             new_string += i
-
     return new_string
+
+
+print(no_c("Best School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
