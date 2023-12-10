5ed7a8f909b7bc34e0103e463eae0688dfeba61b
Meggazord <k_megahed@hotmail.com>
2023-12-10 20:16:51 +0200

    fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-args = sys.argv[1:]
-args_count = len(args)
+    args = sys.argv[1:]
+    args_count = len(args)
 
-if not args:
-    print("0 arguments.")
-else:
-    print("{} {}:".format(args_count, 'argument' if args_count < 2 else 'arguments'))
-    for i, arg in enumerate(args, 1):
-        print("{}: {}".format(i, arg))
+    if not args:
+        print("0 arguments.")
+    else:
+        print("{} {}:".format(args_count,
+                              'argument' if args_count < 2 else 'arguments'))
+        for i, arg in enumerate(args, 1):
+            print("{}: {}".format(i, arg))
