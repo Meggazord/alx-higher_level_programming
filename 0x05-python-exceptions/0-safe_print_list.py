7d6b1ac8fdeb3903c0cecc017b5318fc6a2d2078
Meggazord <k_megahed@hotmail.com>
2023-12-18 22:36:46 +0200

    fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,4 +8,14 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             pass
     print()
-    return count
+
+
+my_list = [1, 2, "Hello", 4, 5]
+
+
+nb_print = safe_print_list(my_list, 3)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
