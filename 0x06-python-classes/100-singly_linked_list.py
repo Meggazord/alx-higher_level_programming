89b7b4f2d123616e0cb810b67b896071d5723c70
Meggazord <k_megahed@hotmail.com>
2023-12-30 00:10:32 +0200

    fixed

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -72,18 +72,3 @@ class SinglyLinkedList:
             current = current.next_node
 
         return (result)
-
-
-sll = SinglyLinkedList()
-sll.sorted_insert(2)
-sll.sorted_insert(5)
-sll.sorted_insert(3)
-sll.sorted_insert(10)
-sll.sorted_insert(1)
-sll.sorted_insert(-4)
-sll.sorted_insert(-3)
-sll.sorted_insert(4)
-sll.sorted_insert(5)
-sll.sorted_insert(12)
-sll.sorted_insert(3)
-print(sll)
