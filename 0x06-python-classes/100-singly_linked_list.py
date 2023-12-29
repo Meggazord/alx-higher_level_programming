462e1338a36a383f209d03124a13893ff0c11e09
Meggazord <k_megahed@hotmail.com>
2023-12-30 00:08:35 +0200

    fixed

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""Defines a class of a Singly Linked List.
+
+A singly linked list is a list which is singly linked. 
+
+"""
+
+
+class Node:
+    """Node class."""
+
+    def __init__(self, data, next_node=None):
+        """Node inititation."""
+        self.data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        """Returns the value of data in a Node."""
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        """Sets the value of data in a Node."""
+        if type(value) != int:
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        """Returns the value of the next Node."""
+        return(self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+        """Creates the next Node."""
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Singly Linked List class."""
+
+    def __init__(self):
+        """Singly Linked List inititation."""
+        self.head = None
+
+    def sorted_insert(self, value):
+        """inserts a new Node into the correct sorted position in the list (increasing order)"""
+        new_node = Node(value)
+        if self.head is None or self.head.data >= new_node.data:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node is not None and current.next_node.data < new_node.data:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        """String representation of the Singly Linked List."""
+        result = ""
+
+        current = self.head
+
+        while(current):
+            result += str(current.data) + "\n"
+            current = current.next_node
+
+        return (result)
+
+
+sll = SinglyLinkedList()
+sll.sorted_insert(2)
+sll.sorted_insert(5)
+sll.sorted_insert(3)
+sll.sorted_insert(10)
+sll.sorted_insert(1)
+sll.sorted_insert(-4)
+sll.sorted_insert(-3)
+sll.sorted_insert(4)
+sll.sorted_insert(5)
+sll.sorted_insert(12)
+sll.sorted_insert(3)
+print(sll)
