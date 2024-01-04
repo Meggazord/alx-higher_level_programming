44b7b5e8700040bf37e325a9feae82829e91c1cb
Meggazord <k_megahed@hotmail.com>
2024-01-05 01:48:48 +0200

    fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle():
 
     def __init__(self, width=0, height=0):
         """object initiation method"""
-        self.__height = height
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -39,11 +39,3 @@ class Rectangle():
             raise ValueError("width must be >= 0")
         else:
             self.__height = value
-
-
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
