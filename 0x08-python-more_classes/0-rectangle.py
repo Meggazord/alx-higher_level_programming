ee4c60dd9e051cd9bab7721bdaf83f71d3b3e732
Meggazord <k_megahed@hotmail.com>
2024-01-05 01:34:18 +0200

    solved

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Rectangle():
+    """ a rectangle class"""
+    pass
+
+#To Do:
+
+- Name mangling and previous solution in previous tasks
+- encapsulation
+- By passing __init__
