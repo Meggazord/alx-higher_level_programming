781e984bcf6981b838bfa136f3046a6f855932ce
Meggazord <k_megahed@hotmail.com>
2024-01-05 22:42:27 +0200

    fioxed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle():
 
     def __init__(self, width=0, height=0):
         """object initiation method"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -36,6 +36,6 @@ class Rectangle():
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
---
+++ 0x08-python-more_classes/ToDo.py
@@ -0,0 +1,5 @@
+# To Do:
+
+- Name mangling and previous solution in previous tasks
+- encapsulation
+- By passing __init__
