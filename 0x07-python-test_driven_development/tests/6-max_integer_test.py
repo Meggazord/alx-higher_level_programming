8fe5b58439085daf503d8237f747b4d88946f786
Meggazord <k_megahed@hotmail.com>
2024-01-15 12:36:42 +0200

    fixed

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 
-import unittest
-import os
-from 6 - max_integer import max_integer
-
 """unittest testing of max_integer function"""
 
+import sys
+sys.path.append(
+    '/Users/Megahed/alx/projects/alx-higher_level_programming/0x07-python-test_driven_development')
+max_integer = __import__('6-max_integer').max_integer
+import unittest
 
-def test_max_integer(self):
-    """unit test of max_integer"""
-    result = max_integer(1, 2, 3)
-    self.assertEqual(result, 3)
+
+class TestMaxInteger(unittest.TestCase):
+    """Unit tests for max_integer function"""
+
+    def test_basic_functionality(self):
+        """basic functionality test of max_integer"""
+        result = max_integer([1, 2, 3, 4])
+        self.assertEqual(result, 4)
+
+    def test_basic_functionality_2(self):
+        """variation # 2 of basic functionalyt testing of max_integer"""
+        result = max_integer([1, 3, 4, 2])
+        self.assertEqual(result, 4)
 
 
 if __name__ == "__main__":
