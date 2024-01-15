46cba24c1eee2d51e4602ba68ce5044c6e331bcf
Meggazord <k_megahed@hotmail.com>
2024-01-15 13:03:43 +0200

    fixed

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,15 +12,45 @@ import unittest
 class TestMaxInteger(unittest.TestCase):
     """Unit tests for max_integer function"""
 
-    def test_basic_functionality(self):
-        """basic functionality test of max_integer"""
+    def test_max_at_end(self):
+        """max_integer at the end"""
         result = max_integer([1, 2, 3, 4])
         self.assertEqual(result, 4)
 
-    def test_basic_functionality_2(self):
-        """variation # 2 of basic functionalyt testing of max_integer"""
+    def test_max_at_beg(self):
+        """max_integer at the beginning"""
+        result = max_integer([4, 2, 3, 1])
+        self.assertEqual(result, 4)
+
+    def test_max_in_mid(self):
+        """max_integer in the middle"""
         result = max_integer([1, 3, 4, 2])
         self.assertEqual(result, 4)
+
+    def test_one_number(self):
+        """function only takes one number"""
+        result = max_integer([3])
+        self.assertEqual(result, 3)
+
+    def test_takes_None(self):
+        """function takes None"""
+        result = max_integer()
+        self.assertEqual(result, None)
+
+    def test_empty_list(self):
+        """function is empty"""
+        result = max_integer()
+        self.assertEqual(result, None)
+
+    def test_takes_one_negative(self):
+        """function takes one negative number"""
+        result = max_integer([1, 3, -4, 2])
+        self.assertEqual(result, 3)
+
+    def test_takes_all_negative(self):
+        """function takes all negative numbers"""
+        result = max_integer([-1, -3, -4, -2])
+        self.assertEqual(result, -1)
 
 
 if __name__ == "__main__":
