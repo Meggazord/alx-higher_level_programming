a8b8addb9a5e2035c800ab2553a7e8b6bc9e6e6f
Meggazord <k_megahed@hotmail.com>
2024-03-04 23:55:06 +0200

    fixed

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,8 +3,8 @@
 -- The script then inserts data into the states table and performs a SELECT query to retrieve the data.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
-
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,7 @@
 -- It uses a subquery to find the state_id of California from the states table and then selects the corresponding cities from the cities table.
 -- Results are sorted in ascending order by cities.id.
 
-SELECT *
+SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id;
+WHERE state_id = (SELECT states.id FROM states WHERE states.name = 'California')
+ORDER BY cities.id ASC;
