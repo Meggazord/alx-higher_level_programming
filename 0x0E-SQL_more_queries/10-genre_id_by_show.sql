b2ab354cb04afdaebf43a744396edacbc925ee28
Meggazord <k_megahed@hotmail.com>
2024-03-05 00:04:12 +0200

    dixed

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,8 @@
 -- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
 
 
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.genre_id = tv_show_genres.genre_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
