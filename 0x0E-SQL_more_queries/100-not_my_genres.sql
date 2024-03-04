ecc2272c1fffb025c56eabb8b5c6cec048967ea3
Meggazord <k_megahed@hotmail.com>
2024-03-05 00:16:51 +0200

    foxde

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,9 +6,8 @@
 -- The database name will be passed as an argument of the mysql command
 
 
-SELECT tv_genres.name AS name
+SELECT DISTINCT tv_genres.name AS name
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id 
-WHERE tv_shows.title != 'Dexter'
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id NOT IN (SELECT tv_genres.name FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_shows.title = 'Dexter')
 ORDER BY tv_genres.name ASC;
