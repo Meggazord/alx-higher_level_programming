e33d33f844e0522c2b549392a09f75c94d28fc7f
Meggazord <k_megahed@hotmail.com>
2024-03-05 00:19:45 +0200

    fixed

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -6,6 +6,6 @@
 
 SELECT tv_shows.title AS title, SUM(tv_show_ratings.rate) AS rating 
 FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id
 ORDER BY tv_show_ratings.rate DESC; 
