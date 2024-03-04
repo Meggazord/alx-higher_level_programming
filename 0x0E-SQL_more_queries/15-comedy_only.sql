01301aa860e10a2d144a3410139b807aa7e34cae
Meggazord <k_megahed@hotmail.com>
2024-03-05 00:10:25 +0200

    foixed

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -5,8 +5,8 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title
+SELECT tv_shows.title AS title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.id = (SELECT tv_genres.id FROM tv_genres WHERE tv_genres.name = 'Comedy')
+JOIN tv_genres ON tv_show_genres.genre_id = (SELECT tv_genres.id FROM tv_genres WHERE tv_genres.name = 'Comedy')
 ORDER BY tv_shows.title ASC;
