7785c08349d7b404391f293dd2aa4242d9669519
Meggazord <k_megahed@hotmail.com>
2024-03-05 00:12:44 +0200

    fixde

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -8,5 +8,5 @@
 SELECT tv_shows.title AS title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = (SELECT tv_genres.id FROM tv_genres WHERE tv_genres.name = 'Comedy')
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
