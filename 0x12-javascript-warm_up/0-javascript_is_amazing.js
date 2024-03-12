813e2f64e4124bdae7e4a05d8b9ab3f763fb4bd7
Meggazord <k_megahed@hotmail.com>
2024-03-13 00:23:52 +0200

    fixeD

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-// 0-javascript_is_amazing.js
-// Create a constant variable called myVar with the value "JavaScript is amazing"
-
 const myVar = "JavaScript is amazing";
 console.log(myVar);
