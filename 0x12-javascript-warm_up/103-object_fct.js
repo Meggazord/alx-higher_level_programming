58b57066915445ca23c871eae664cb7993943828
Meggazord <k_megahed@hotmail.com>
2024-03-13 03:25:08 +0200

    foxed

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -10,3 +10,10 @@ console.log(myObject);
 myObject.incr = function () {
   this.value++;
 };
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
