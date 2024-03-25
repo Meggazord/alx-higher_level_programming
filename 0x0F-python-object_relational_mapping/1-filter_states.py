fc399f3ed931496daf9c113694eb6c2f21940cb6
Meggazord <k_megahed@hotmail.com>
2024-03-25 22:38:19 +0200

    fixed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,7 +2,6 @@
 """
 Script that filters all states from the database hbtn_0e_0_usa
 """
-
 if __name__ == "__main__":
     import MySQLdb
     import sys
@@ -12,9 +11,8 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3],
         port=3306)
-
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
